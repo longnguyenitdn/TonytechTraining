1bfc36e9cecec1e1f8621380b562960d42e9de99
longnguyencodegym <longnguyen.tourism@gmail.com>
2022-10-28 14:02:05 +0700

    bai tap 28-10

--- bai_tap2/bai_tap2.js
+++ bai_tap2/bai_tap2.js
@@ -1,17 +1,17 @@
-function open_modal(){
-
-  let element= document.getElementById("modal-wrapper");
-  element.classList.remove("hide");
-
-}
-function close_modal(){
-   
-   let element=document.getElementById("modal");
-   element.classList.add("hide");
-   
-}
 let persons=[];
-display(persons);
+
+function open_modal(){
+  let element= document.getElementById("modal_wrapper");
+  element.classList.remove("hide");
+}
+
+function close_modal(){ 
+   clearForm();
+   let element=document.getElementById("modal_wrapper");
+   element.classList.add("hide");  
+}
+
+
 function display(inputPersons){
    let tableString='<table class="table">'+
    '<tbody>'+
@@ -25,36 +25,57 @@ function display(inputPersons){
          '<th scope="col">Phone</th>'+
          '<th colspan="4"></th>'+
       '</tr>';
-      // for (let i=0;i<inputPersons.lenghth;i++) {
-      //    tableString+='<tr>'+
-      //    '<th class="check-box">'+
-      //       '<input class="form-check-input" type="checkbox">'+
-      //    '</th>'+
-      //    '<th scope="row"><img class="img_border" src="https://cho77.com/wp-content/uploads/2022/05/lionel-messi-1024x576.jpg" alt="1"></th>'+
-      //    '<td>Messi</td>'+
-      //    '<td>m@gmail</td>'+
-      //    '<td>00000000</td>'+
-      //    '<td><i class="bi bi-pencil-square"></i></td>'+
-      //    '<td><i class="bi bi-trash"></i></td>'+
-      // '</tr>';
-      // }
+      for (let i=0 ; i<inputPersons.length ; i++) {
+         tableString += '<tr>'+
+         '<th class="check-box">'+
+            '<input class="form-check-input" type="checkbox">'+
+         '</th>'+
+         '<th scope="row"><img class="img_border" src="'+URL.createObjectURL(inputPersons[i].photo[0])+'" alt="1"></th>'+
+         '<td>'+inputPersons[i].name+'</td>'+
+         '<td>'+inputPersons[i].email+'</td>'+
+         '<td>'+inputPersons[i].phone+'</td>'+
+         '<td><i class="bi bi-pencil-square"></i></td>'+
+         '<td><i class="bi bi-trash"></i></td>'+
+      '</tr>';
+      }
       tableString+='</tbody>';
    tableString+='</table>';
    document.getElementById("display").innerHTML=tableString;
-
-
 }
+
 function add(){
-   let name=document.getElementById("name")?.value;
-   let email=document.getElementById("email")?.value;
-   let phone=document.getElementById("phone")?.value;
-   let photo=document.getElementById("photo")?.value;
+   let name=document.getElementById("name").value;
+   let email=document.getElementById("email").value;
+   let phone=document.getElementById("phone").value;
+   let photo=document.getElementById("photo").files;
    let person={};
    person.name=name;
    person.email=email;
    person.phone=phone;
    person.photo=photo;
    persons.push(person);
-   console.log(persons);
-   
+   display(persons);  
+   close_modal();
 }
+
+
+function clearForm(){
+   let form=document.getElementById("my_form");
+   form.reset()
+}
+
+
+
+document.querySelector("#my_form").addEventListener("submit", function(e){
+   
+   if(!e.isValid){
+       e.preventDefault();    //stop form from submitting
+   }
+
+   add();
+   clearForm();
+});
+
+
+
+
