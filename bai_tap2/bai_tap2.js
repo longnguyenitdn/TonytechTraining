f772c73013ee88856ac5022d8d05218c1ae0886d
longnguyencodegym <longnguyen.tourism@gmail.com>
2022-10-31 16:52:01 +0700

    update lan 2 31-10

--- bai_tap2/bai_tap2.js
+++ bai_tap2/bai_tap2.js
@@ -1,6 +1,6 @@
 let persons = [];
-let isAdd=true;
-
+let isAdd = true;
+let indexEdit = null;
 function open_modal() {
    document.getElementById("modal_wrapper").classList.remove("hide");
 }
@@ -10,9 +10,6 @@ function close_modal() {
    document.getElementById("modal_wrapper").classList.add("hide");
 
 }
-
-
-
 
 function display(inputPersons) {
    let tableString = '<table class="table">' +
@@ -36,8 +33,16 @@ function display(inputPersons) {
          '<td>' + inputPersons[i].name + '</td>' +
          '<td>' + inputPersons[i].email + '</td>' +
          '<td>' + inputPersons[i].phone + '</td>' +
-         '<td><i onclick="updateInput(' + i + ')" class="bi bi-pencil-square"></i></td>' +
-         '<td><i onclick="remove(' + i + ')" class="bi bi-trash"></i></td>' +
+         '<td><i onclick="updateInput(' + i + ')" class="bi bi-pencil-square"></i> </td>' +
+         '<div>' +
+         '<td ><i onclick="remove(' + i + ')" class="bi bi-trash"></i>' +
+         '<div class="remove-conf btn btn-primary">' +
+         '<p>Delete it?</p>' +
+         '<button id="delete" type="submit" class="btn btn-danger">OK</button>' +
+         '<button type="button" class="btn btn-danger">Cancel</button>' +
+         '</div>'
+      '</td>' +
+         '</div>' +
          '</tr>';
    }
    tableString += '</tbody>';
@@ -71,42 +76,49 @@ function remove(index) {
    }
 }
 
-function add(key) {
-   if(key){
+function add() {
+   if (isAdd) {
       let person = getValue();
-   persons.push(person);
-   display(persons);
-   close_modal();
-   }else{
-      update();
+      persons.push(person);
+      display(persons);
+      close_modal();
+   } else {
+      update(indexEdit);
    }
-   
+
 }
 
 function update(index) {
-   let person=getValue();
+
+   let person = getValue();
+
    persons[index].name = person.name;
    persons[index].email = person.email;
    persons[index].phone = person.phone;
-   persons[index].photo = person.photo;
-   display(persons);
+   persons[index].photo = person.file[0];
+   display(persons)
    close_modal();
 }
 
 function updateInput(index) {
+   console.log(persons);
+   console.log(persons[index])
+   isAdd = false;
+   indexEdit = index;
    open_modal();
    document.getElementById("name").value = persons[index].name;
    document.getElementById("email").value = persons[index].email;
    document.getElementById("phone").value = persons[index].phone;
-   document.getElementById("photo").files= persons[index].files[0];
+   document.getElementById("photo").files = files[0];
+
 }
 
 const photo = document.getElementById('photo');
 const image = document.getElementById('img-preview');
 photo.addEventListener('change', (e) => {
    if (e.target.files.length) {
-       const src = URL.createObjectURL(e.target.files[0]);
-       image.src = src;
+      const src = URL.createObjectURL(e.target.files[0]);
+      image.src = src;
    }
 });
 
