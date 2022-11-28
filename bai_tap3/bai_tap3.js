ea8c6687912b4ba57e4de4c4686aa21b4b0a0b61
longnguyencodegym <longnguyen.tourism@gmail.com>
2022-11-28 14:12:28 +0700

    bai tap 28-11

--- bai_tap3/bai_tap3.js
+++ bai_tap3/bai_tap3.js
@@ -1,19 +1,29 @@
+let isOpen = false;
 
+const onclickOpenDetailModal = () => {
 
-const onclickOpenDetailModal =()=>{
-   
-      document.getElementById("input_note_cover").classList.add("hiden");
-      document.getElementById("input_note_detail").classList.remove("hiden");
+   document.getElementById("input_note_cover").classList.add("hiden");
+   document.getElementById("input_note_detail").classList.remove("hiden");
 }
-const closeDetailModal =()=>{
+const closeDetailModal = () => {
    document.getElementById("input_note_cover").classList.remove("hiden");
    document.getElementById("input_note_detail").classList.add("hiden");
 }
 
-document.getElementById("input_note").addEventListener("click",onclickOpenDetailModal);  
+
 
 document.body.addEventListener('click', e => {
-   if (!e.target.classList.contains('skip') ) {    
-      closeDetailModal();
+   if (!isOpen) {
+      const openNote = document.getElementById("input_note_cover");
+      if (openNote.contains(e.target)) {
+         onclickOpenDetailModal();
+         isOpen=true;
+      }
+   }else{
+      const closeNote= document.getElementById("input_note_detail");
+      if(!closeNote.contains(e.target)){
+         closeDetailModal();
+         isOpen=false;
+      }
    }
 });
