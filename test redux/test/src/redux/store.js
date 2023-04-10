365b0d6cc09bdae6735d64e990139e916e332018
longnguyencodegym <longnguyen.tourism@gmail.com>
2023-04-10 16:44:45 +0700

    remove folder test_redux

--- test redux/test/src/redux/store.js
+++
@@ -1,6 +0,0 @@
-import { applyMiddleware, createStore } from "redux";
-import thunk from "redux-thunk";
-import rootReducer from "./reducers";
-const middleWare = [thunk];
-const store = createStore(rootReducer, applyMiddleware(...middleWare));
-export default store;
